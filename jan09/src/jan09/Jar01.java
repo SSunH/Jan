13d7e53697f4b1c475cb9295969a31591c839622
admin <admin@DESKTOP-V6LS8OT>
2023-01-09 17:28:32 +0900

    jar, war, ear

---
+++ jan09/src/jan09/Jar01.java
@@ -0,0 +1,18 @@
+package jan09;
+ // jar, war, ear
+public class Jar01 {
+/*
+ * 
+ *  jar(Java Archive) .class
+ *  
+ *  war(Web) 웹 배포형태 (압축형태)
+ *  
+ *  ear(Enterprise) 
+ *  
+ */	
+	
+	public static void main(String[] args) {
+		System.out.println("묶어보겠습니다.");
+	}
+
+}
